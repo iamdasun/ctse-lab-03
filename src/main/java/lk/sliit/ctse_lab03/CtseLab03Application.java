93e8434c38925d9170722783626a193868c84b03
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2026-02-15 12:50:39 +0530

    first commit

---
+++ src/main/java/lk/sliit/ctse_lab03/CtseLab03Application.java
@@ -0,0 +1,13 @@
+package lk.sliit.ctse_lab03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtseLab03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtseLab03Application.class, args);
+	}
+
+}
---
+++ src/test/java/lk/sliit/ctse_lab03/CtseLab03ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sliit.ctse_lab03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtseLab03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
